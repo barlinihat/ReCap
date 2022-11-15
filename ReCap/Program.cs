841d6d8bb9490b56fd4d2dbdfd7030b4329d9248
barlinihat <56547401+barlinihat@users.noreply.github.com>
2022-11-15 21:28:30 +0300

    Proje dosyası ekle.

---
+++ ReCap/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReCap;
+
+Console.WriteLine("Hello, World!");
+
+
+Product product = new Product();
+
+product.Id = 123;
+
+BasketManager basketManager = new BasketManager();
+basketManager.Add();

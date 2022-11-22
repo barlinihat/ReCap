458218dca6fd6cc61ae4cb72008137f623fcfde0
barlinihat <56547401+barlinihat@users.noreply.github.com>
2022-11-22 22:28:39 +0300

    Game project added.

---
+++ GameProject/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using GameProject;
+
+GamerManager gamerManager = new GamerManager(new UserValidationManager());
+gamerManager.Add(new Gamer { Id = 1, BirthYear = 1997, FirstName = "Robert", LastName = "Marie", IdentityNumver = 12345 });
+
+
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1997 && gamer.FirstName == "Robert" && gamer.LastName == "Marie" && gamer.IdentityNumver == 12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
